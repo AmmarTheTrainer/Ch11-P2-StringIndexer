e85077dbb6b30c335fb2729a7d76a91fc019d6ab
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-10-07 17:15:14 +0500

    code update 1

---
+++ Ch11-P2-StringIndexer/CustomPeopleList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11_P2_StringIndexer
+{
+    class CustomPeopleList
+    {
+        public List<Person> peopleList { get; set; }
+
+        public CustomPeopleList()
+        {
+            peopleList = new List<Person>();
+        }
+
+        public Person this[string name]
+        {
+            get => (Person)peopleList[int.Parse(name)];
+            //set => peopleList.Add(int.Parse(name));
+        }
+    }
+}
--- Ch11-P2-StringIndexer/Program.cs
+++ Ch11-P2-StringIndexer/Program.cs
@@ -10,6 +10,39 @@ namespace Ch11_P2_StringIndexer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Indexers *****\n");
+
+
+            #region Indexing with Strings in C#
+
+            //CustomPeopleList PeopleList = new CustomPeopleList()
+            //{
+            //    peopleList = { new Person {  FirstName = "Ammar1" , LastName = "Shaukat" , Age = 22},
+            //                   new Person {  FirstName = "Ammar1" , LastName = "Shaukat" , Age = 22},
+            //                   new Person {  FirstName = "Ammar1" , LastName = "Shaukat" , Age = 22}
+            //                    },
+            //};
+
+            ////PeopleList["3"] = new Person() { FirstName = "ammar4"}
+
+            //Person person = PeopleList["2"];
+            //Console.WriteLine(person);
+            //Console.WriteLine();
+
+            #endregion
+
+            PersonCollection myPeople = new PersonCollection();
+
+            myPeople["Homer"] = new Person("Homer", "Simpson", 40);
+            myPeople["Marge"] = new Person("Marge", "Simpson", 38);
+            
+            // Get "Homer" and print data.
+            Person homer = myPeople["Homer"];
+
+            Console.WriteLine(homer.ToString());
+
+
+            Console.ReadLine();
         }
     }
 }
